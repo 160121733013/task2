57bac6ed3cd22941ee5ce2b47c99c7b2fa4fe8b8
160121733013 <117197395+160121733013@users.noreply.github.com>
2023-10-28 08:45:52 +0530

    Add files via upload

---
+++ Project/shopping.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Cotton Silk Saree',
+    category : 'Sarees',
+    rating : 4.1,
+    price: 499,
+    img: 'images/Sarees/saree.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Flat Saree',
+    category : 'Sarees',
+    rating : 4.2,
+    price: 599,
+    img: 'images/Sarees/saree1.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Pure Silk Embellished',
+    category : 'Sarees',
+    rating : 4.0,
+    price: 759,
+    img: 'images/Sarees/saree2.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Cotton Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 399,
+    img: 'images/Sarees/saree4.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Designer Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 599,
+    img: 'images/Sarees/saree5.jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Banaras Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 666,
+    img: 'images/Sarees/saree6.jpeg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Styllish Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 999,
+    img: 'images/Sarees/saree7.jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Designer Silk Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 789,
+    img: 'images/Sarees/saree8.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Pattu Saree',
+    category : 'Sarees',
+    rating : 4.3,
+    price: 999,
+    img: 'images/Sarees/saree9.jpeg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Short Top',
+    category : 'Tops',
+    rating : 4.3,
+    price: 1099,
+    img: 'images/Tops/t1.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Jeens Top',
+    category : 'Tops',
+    rating : 4.3,
+    price: 889,
+    img: 'images/Tops/t2.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Designer Top',
+    category : 'Tops',
+    rating : 4.3,
+    price: 756,
+    img: 'images/Tops/t3.jpeg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Formal Top',
+    category : 'Tops',
+    rating : 4.3,
+    price: 894,
+    img: 'images/Tops/t4.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Modern Shot Top',
+    category : 'Tops',
+    rating : 4.3,
+    price: 567,
+    img: 'images/Tops/t5.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Short Top for Girls',
+    category : 'Tops',
+    rating : 4.3,
+    price: 620,
+    img: 'images/Tops/t6.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Frock',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 15,
+    img: 'images/chinese/k1.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Boysware',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/k2.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'PantShirt for boys',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 15,
+    img: 'images/chinese/k3.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Jeensware for boys',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chinese/k4.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Dress for girls',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/k5.jpeg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Traditional ware ',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chinese/k6.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Tshirt for boys',
+    age: '32',
+    category : 'Kidsware',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chinese/k7.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Laptop',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 8,
+    img: 'images/vegetable/7.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Designed Watch',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/6.jpeg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Watch',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 5,
+    img: 'images/vegetable/5.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'shoes',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/4.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Sandles',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 10,
+    img: 'images/vegetable/3.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Flats',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/2.jpeg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Hand Bag',
+    category : 'Accessories',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/1.jpeg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'suit jacket',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 8,
+    img: 'images/paneer/m1.jpeg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Formal Shirt',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 7,
+    img: 'images/paneer/m2.jpeg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Kurtha',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 5,
+    img: 'images/paneer/m3.jpeg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Shirt',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 16,
+    img: 'images/paneer/m4.jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Tshirt',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 14,
+    img: 'images/paneer/m5.jpeg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'hud jacket',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 10,
+    img: 'images/paneer/m6.jpeg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Plain Shirt',
+    category : 'Mensware',
+    rating : 4.3,
+    price: 8,
+    img: 'images/paneer/m7.jpeg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'White Dress',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 18,
+    img: 'images/south indian/1.jpeg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Kurtha set for men',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 20,
+    img: 'images/south indian/2.jpeg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 12,
+    img: 'images/south indian/3.jpeg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 10,
+    img: 'images/south indian/4.jpeg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 15,
+    img: 'images/south indian/5.jpeg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 40,
+    img: 'images/south indian/6.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 25,
+    img: 'images/south indian/7.jpeg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Traditional ware',
+    category : 'TraditionalWare',
+    rating : 4.3,
+    price: 34,
+    img: 'images/south indian/8.jpeg',
+    quantity: 1
+},
+]
+
+export {foodItem};
